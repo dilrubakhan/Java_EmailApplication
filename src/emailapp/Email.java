3f1d46756331e83f1471baaacd1dea36fd7ea309
Dilruba Khan <rubatanisa@yahoo.ie>
2021-11-03 18:57:17 +0000

    Setting variable, constructor and genarating email method

---
+++ src/emailapp/Email.java
@@ -0,0 +1,50 @@
+package emailapp;
+
+public class Email {
+    public Scanner sc = new Scanner(System.in);
+
+    //Setting variables
+    private String firstName;
+    private String lastName;
+    private String department;
+    private String email;
+    private String password;
+    private int mailCapacity = 500;
+    private String alterEmail;
+
+    public Email(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("New Employee: "+ this.firstName + this.lastName);
+
+    }
+
+    public String generateEmail(){
+        return this.firstName.toLowerCase()+ "." + this.lastName.toLowerCase()+ "@" + this.department.toLowerCase()+ ".company.com";
+    }
+    //Asking for department
+    private String setDept() {
+        System.out.println("Department codes \n1 for Sales \n2 for Development \3 for Accounting \n0 for none");
+        boolean flag = false;
+
+        do {
+            System.out.println("Please enter department code:");
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    return "Sales";
+                case 2:
+                    return "Development";
+                case 3:
+                    return "Accounting";
+                case 0:
+                    return "None";
+                default:
+                    System.out.println("Invalid choice. Please choose it again");
+            }
+        } while (!flag);
+
+        return null;
+    }
+
+}
